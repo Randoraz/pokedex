ce4ae9a376072eb6ca76be8f1e7abec2553cc686
Randoraz <97787227+Randoraz@users.noreplay.github.com>
2022-07-27 17:36:51 +0200

    Add type buttons and type images

--- src/components/PokemonInfo/PokemonInfo.js
+++ src/components/PokemonInfo/PokemonInfo.js
@@ -6,6 +6,25 @@ import { getPokemon, captalizeFirstLetter } from "../../util/PokeAPI";
 
 import './PokemonInfo.css';
 
+import normal from '../../images/Types/GO_Normal.webp';
+import fighting from '../../images/Types/GO_Fighting.webp';
+import flying from '../../images/Types/GO_Flying.webp';
+import poison from '../../images/Types/GO_Poison.webp';
+import ground from '../../images/Types/GO_Ground.webp';
+import rock from '../../images/Types/GO_Rock.webp';
+import bug from '../../images/Types/GO_Bug.webp';
+import ghost from '../../images/Types/GO_Ghost.webp';
+import steel from '../../images/Types/GO_Steel.webp';
+import fire from '../../images/Types/GO_Fire.webp';
+import water from '../../images/Types/GO_Water.webp';
+import grass from '../../images/Types/GO_Grass.webp';
+import electric from '../../images/Types/GO_Electric.webp';
+import psychic from '../../images/Types/GO_Psychic.webp';
+import ice from '../../images/Types/GO_Ice.webp';
+import dragon from '../../images/Types/GO_Dragon.webp';
+import dark from '../../images/Types/GO_Dark.webp';
+import fairy from '../../images/Types/GO_Fairy.webp';
+
 export function PokemonInfo() {
     const [pokemon, setPokemon] = useState({});
     const [foundPokemon, setFoundPokemon] = useState(false);
@@ -22,7 +41,7 @@ export function PokemonInfo() {
                     setPokemon(pokemon);
                     setFoundPokemon(true);
                     setTerm(pokemon.name);
-                    //console.log(pokemon);
+                    console.log(pokemon);
                 } else {
                     setFoundPokemon(false);
                 }
@@ -41,12 +60,69 @@ export function PokemonInfo() {
         setTerm(e.target.value);
     };
 
+    const type1Button = () => {
+        if(Object.keys(pokemon).length !== 0)
+            history.push(`/types/${pokemon.types[0].type.name}`);
+    };
+
+    const type2Button = () => {
+        if(Object.keys(pokemon).length !== 0 && pokemon.types.length > 1)
+            history.push(`/types/${pokemon.types[1].type.name}`);
+    };
+
     const prevButton = () => {
-        history.push(`/pokedex/${pokemon.id - 1}`);
+        if(Object.keys(pokemon).length !== 0 && pokemon.id > 1)
+            history.push(`/pokedex/${pokemon.id - 1}`);
     };
 
     const nextButton = () => {
-        history.push(`/pokedex/${pokemon.id + 1}`);
+        if(Object.keys(pokemon).length !== 0)
+            history.push(`/pokedex/${pokemon.id + 1}`);
+        else
+            history.push(`/pokedex/1`);
+    };
+
+    const displayTypeImage = (type) => {
+        switch(type.type.name) {
+            case 'normal':
+                return <img src={normal} alt={type.type.name} key={type.slot} className={`type${type.slot}`} />;
+            case 'fighting':
+                return <img src={fighting} alt={type.type.name} key={type.slot} className={`type${type.slot}`} />;
+            case 'flying':
+                return <img src={flying} alt={type.type.name} key={type.slot} className={`type${type.slot}`} />;
+            case 'poison':
+                return <img src={poison} alt={type.type.name} key={type.slot} className={`type${type.slot}`} />;
+            case 'ground':
+                return <img src={ground} alt={type.type.name} key={type.slot} className={`type${type.slot}`} />;
+            case 'rock':
+                return <img src={rock} alt={type.type.name} key={type.slot} className={`type${type.slot}`} />;
+            case 'bug':
+                return <img src={bug} alt={type.type.name} key={type.slot} className={`type${type.slot}`} />;
+            case 'ghost':
+                return <img src={ghost} alt={type.type.name} key={type.slot} className={`type${type.slot}`} />;
+            case 'steel':
+                return <img src={steel} alt={type.type.name} key={type.slot} className={`type${type.slot}`} />;
+            case 'fire':
+                return <img src={fire} alt={type.type.name} key={type.slot} className={`type${type.slot}`} />;
+            case 'water':
+                return <img src={water} alt={type.type.name} key={type.slot} className={`type${type.slot}`} />;
+            case 'grass':
+                return <img src={grass} alt={type.type.name} key={type.slot} className={`type${type.slot}`} />;
+            case 'electric':
+                return <img src={electric} alt={type.type.name} key={type.slot} className={`type${type.slot}`} />;
+            case 'psychic':
+                return <img src={psychic} alt={type.type.name} key={type.slot} className={`type${type.slot}`} />;
+            case 'ice':
+                return <img src={ice} alt={type.type.name} key={type.slot} className={`type${type.slot}`} />;
+            case 'dragon':
+                return <img src={dragon} alt={type.type.name} key={type.slot} className={`type${type.slot}`} />;
+            case 'dark':
+                return <img src={dark} alt={type.type.name} key={type.slot} className={`type${type.slot}`} />;
+            case 'fairy':
+                return <img src={fairy} alt={type.type.name} key={type.slot} className={`type${type.slot}`} />;
+            default:
+                break;
+        }        
     };
 
     return (
@@ -55,10 +131,13 @@ export function PokemonInfo() {
             <h2>{captalizeFirstLetter(pokemon.name)}</h2>
             <p className="p id">{pokemon.id}</p>
             <img src={pokemon.sprites.versions['generation-v']['black-white'].animated.front_default} alt={pokemon.name} />
-            <div>
-                {pokemon.types.map(type => {
-                    return <p className={`type${type.slot}`} key={type.slot}>{captalizeFirstLetter(type.type.name)}</p>;
-                })}
+            <div className="types">
+                <div className="cell-div">
+                    <button className="type-button" onClick={type1Button}>{displayTypeImage(pokemon.types[0])}</button>
+                </div>
+                <div className="cell-div">
+                    <button className="type-button" onClick={type2Button}>{pokemon.types.length > 1 && displayTypeImage(pokemon.types[1])}</button>
+                </div>
             </div>
             <form className="form" onSubmit={handleSubmit}>
                 <input
@@ -73,6 +152,14 @@ export function PokemonInfo() {
         </div>
         :
         <div className="pokemon-info">
+            <div className="types">
+                <div className="cell-div">
+                    <button className="type-button"></button>
+                </div>
+                <div className="cell-div">
+                    <button className="type-button"></button>
+                </div>
+            </div>
             <form className="form" onSubmit={handleSubmit}>
                 <input
                     className="input"
@@ -81,6 +168,8 @@ export function PokemonInfo() {
                     placeholder='name or number'
                     onChange={handleChange} />
             </form>
+            <button className="button prev" onClick={prevButton}>&lt; Prev</button>
+            <button className="button next" onClick={nextButton}>Next &gt;</button>
         </div>
     );
 }
